2d427f2fdb473d724fe45dbd6d6ad277308dc929
Veena S Kumar <38048590+veenasnair18@users.noreply.github.com>
2020-05-24 15:09:08 +0530

    Add files via upload

---
+++ bottom25.py
@@ -0,0 +1,30 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import imdb
+>>> i=imdb.IMDb()
+>>> s=i.get_bottom100_movies()
+>>> for j in range(20):
+	print(s[j])
+
+	
+Disaster Movie
+Superbabies: Baby Geniuses 2
+Manos: The Hands of Fate
+Code Name: K.O.Z.
+Birdemic: Shock and Terror
+The Hottie & the Nottie
+Saving Christmas
+House of the Dead
+Pledge This!
+Son of the Mask
+Epic Movie
+Turks in Space
+Alone in the Dark
+From Justin to Kelly
+Battlefield Earth
+Going Overboard
+Dragonball Evolution
+Daniel the Wizard
+Gigli
+Glitter
+>>> 
